208c04a7887d511f415dadc40e9bbb319f4d0647
Riandito <51911016+WorkingDoc@users.noreply.github.com>
2022-09-26 22:39:59 +1000

    deleting unnecessary file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-import ('./bootstrap');
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import './styles/main.css';
+
+const mount = (el, identity) => {
+    ReactDOM.render(<App identity={identity} />,
+    el
+    );
+}
+
+// if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector('#root-MentalHealth')
+
+    if (devRoot) {
+        mount(devRoot);
+    }
+// }
+
+export { mount };
+
